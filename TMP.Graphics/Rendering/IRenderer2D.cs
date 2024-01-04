5a6e3025612c33257030b640c12cbac93701987a
Tristan Pearce <tristanpearce24@gmail.com>
2024-01-04 22:23:47 +0000

    Initial IRenderer2D interface

---
+++ TMP.Graphics/Rendering/IRenderer2D.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMP.Graphics.Rendering
+{
+    public interface IRenderer2D
+    {
+        Color FillColor { get; set; }
+        Color OutlineColor { get; set; }
+
+        float OutlineThickness { get; set; }
+
+        void Draw(Line line);
+        void Draw(Ellipse ellipse);
+        void Draw(Rectangle rectangle);
+        void Draw(Polygon polygon);
+        void Draw(Image image);
+    }
+}
---
+++ TMP.Graphics/Rendering/Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMP.Graphics.Rendering
+{
+    public struct Line
+    {
+        public Vector2 Start;
+        public Vector2 End;
+
+        public Line(Vector2 start, Vector2 end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+}
