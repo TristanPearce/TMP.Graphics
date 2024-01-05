45f1afdd028bc050277fbcce8d2fb573e848197e
Tristan Pearce <tristanpearce24@gmail.com>
2024-01-05 19:01:08 +0000

    Impl rect (broken)

--- TMP.Graphics/Rendering/Rectangle.cs
+++ TMP.Graphics/Rendering/Rectangle.cs
@@ -8,5 +8,36 @@ namespace TMP.Graphics
 {
     public struct Rectangle
     {
+
+        public float X {  get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }  
+
+        public float Left
+        {
+            readonly get => X - Width / 2;
+            set => X = value + Width / 2;
+        }
+
+        public float Right
+        {
+            readonly get => X + Width / 2;
+            set => X = value - Width / 2;
+        }
+
+        public float Top
+        {
+            readonly get => Y - Height / 2;
+            set => Y = value - Height / 2;
+        }
+
+
+        public float Bottom
+        {
+            readonly get => Y + Height / 2;
+            set => Height = value + Height / 2;
+        }
+
     }
 }
