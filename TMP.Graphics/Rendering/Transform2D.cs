9a7fed03ba7eb2d4abbad15e80df706943924a4c
Tristan Pearce <tristanpearce24@gmail.com>
2024-01-05 23:47:31 +0000

    Impl Transform2D

---
+++ TMP.Graphics/Rendering/Transform2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMP.Graphics.Rendering
+{
+    public sealed class Transform2D
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Scale { get; set; }
+        public float Rotation { get; set; } 
+
+        public Transform2D() 
+        {
+
+        }
+
+        public static explicit operator Matrix3x2(Transform2D transform)
+        {
+            return 
+                Matrix3x2.CreateTranslation(transform.Position) *
+                Matrix3x2.CreateScale(transform.Position) *
+                Matrix3x2.CreateRotation(transform.Rotation);
+        }
+    }
+}
