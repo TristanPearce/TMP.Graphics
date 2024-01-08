93a90a40aed01027b7ed708bc36f0605e9eb1c4c
Tristan Pearce <tristanpearce24@gmail.com>
2024-01-08 22:46:47 +0000

    refactor shape tests

--- TMP.Graphics.Tests.Unit/Shapes/EllipseTests.cs
+++ TMP.Graphics.Tests.Unit/Shapes/EllipseTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMP.Graphics.Rendering;
+using TMP.Graphics.Rendering.Shapes;
 
 namespace TMP.Graphics.Tests.Unit.Shapes
 {
--- TMP.Graphics.Tests.Unit/Shapes/RectangleTests.cs
+++ TMP.Graphics.Tests.Unit/Shapes/RectangleTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMP.Graphics.Rendering.Shapes;
 
 namespace TMP.Graphics.Tests.Unit.Shapes
 {
