92251a0af53318d616927b5a34557243a27afff5
Tristan Pearce <tristanpearce24@gmail.com>
2024-01-12 16:55:26 +0000

    recalculate matrix only when needed

--- TMP.Graphics/Rendering/Transform2D.cs
+++ TMP.Graphics/Rendering/Transform2D.cs
@@ -9,25 +9,28 @@ namespace TMP.Graphics
 {
     public sealed class Transform2D
     {
+        private bool _shouldRecalculateMatrix;
+        private Matrix3x2 _backingMatrix;
+
         private Vector2 _translation;
         public Vector2 Translation 
         {
             get => _translation; 
-            set => _translation = value;
+            set => SetTranslation(value.X, value.Y);
         }
 
         private Vector2 _scale;
         public Vector2 Scale 
         { 
             get => _scale;
-            set => _scale = value; 
+            set => SetScale(value.X, value.Y);
         }
 
         private float _rotation;
         public float Rotation 
         {
             get => _rotation;
-            set => _rotation = value;
+            set => SetRotation(value);
         }
 
         public Transform2D() 
@@ -37,28 +40,46 @@ namespace TMP.Graphics
             Rotation = 0f;
         }
 
+        private void InvalidateMatrix()
+        {
+            _shouldRecalculateMatrix = true;
+        }
+
+        private void RecalculateMatrix()
+        {
+            _backingMatrix = Matrix3x2.CreateScale(_scale) *
+                Matrix3x2.CreateRotation(_rotation) *
+                Matrix3x2.CreateTranslation(_translation);
+            _shouldRecalculateMatrix = true;
+        }
+
         #region Translation
 
         public void SetTranslation(float x, float y)
         {
             _translation.X = x;
             _translation.Y = y;
+
+            InvalidateMatrix();
         }
 
         public void SetTranslationX(float x)
         {
             _translation.X = x;
+            InvalidateMatrix();
         }
 
         public void SetTranslationY(float y)
         {
             _translation.Y = y;
+            InvalidateMatrix();
         }
 
         public void TranslateBy(float dX, float dY)
         {
             _translation.X += dX;
             _translation.Y += dY;
+            InvalidateMatrix();
         }
 
         #endregion
@@ -69,16 +90,19 @@ namespace TMP.Graphics
         {
             _scale.X = x;
             _scale.Y = y;
+            InvalidateMatrix();
         }
 
         public void SetScaleX(float x)
         {
             _scale.X = x;
+            InvalidateMatrix();
         }
 
         public void SetScaleY(float y)
         {
             _scale.Y = y;
+            InvalidateMatrix();
         }
 
         public void ScaleBy(float dX, float dY, ScaleByMethod method)
@@ -100,6 +124,7 @@ namespace TMP.Graphics
                 default:
                     throw new NotSupportedException($"ScaleByMethod ({method}) is not supported.");
             }
+            InvalidateMatrix();
         }
 
         public enum ScaleByMethod
@@ -122,19 +147,26 @@ namespace TMP.Graphics
 
         #region Rotation
 
+        public void SetRotation(float angle)
+        {
+            _rotation = angle;
+            InvalidateMatrix();
+        }
+
         public void RotateBy(float dAngle)
         {
             _rotation += dAngle;
+            InvalidateMatrix();
         }
 
         #endregion
 
         public static explicit operator Matrix3x2(Transform2D transform)
         {
-            return
-                Matrix3x2.CreateScale(transform.Scale) *
-                Matrix3x2.CreateRotation(transform.Rotation) *
-                Matrix3x2.CreateTranslation(transform.Translation);
+            if (transform._shouldRecalculateMatrix)
+                transform.RecalculateMatrix();
+
+            return transform._backingMatrix;
         }
     }
 }
